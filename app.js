0ff3a2f4fdc8bb37e7cbfdc0ea8f3e2ee6d97ce2
Robert Wearden <robertwearden2@gmail.com>
2021-05-09 23:16:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const countDown = () => {
+  const targetDate = new Date('June 11, 2021 17:00:00').getTime();
+  const now = new Date().getTime();
+  const gap = targetDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSeconds = Math.floor((gap % minute) / second);
+
+  document.querySelector('.day').innerText = textDay;
+  document.querySelector('.hour').innerText = textHour;
+  document.querySelector('.minute').innerText = textMinute;
+  document.querySelector('.second').innerText = textSeconds;
+};
+
+setInterval(countDown, 1000);
